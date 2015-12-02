3f2742e7ca124086d0ca499a6c6e3cb1a6833f97
Laszlo Korte <me@laszlokorte.de>
2015-12-02 21:44:13 +0100

    extract curve calculations

--- common.js
+++ common.js
@@ -28,7 +28,42 @@
     while (node.lastChild) node.removeChild(node.lastChild);
   }
 
+  var curvedConnection = function(from, to, offset) {
+    var deltaX = to.x - from.x;
+    var deltaY = to.y - from.y;
 
+    var rad = Math.atan2(deltaX, deltaY);
+    var radExit = rad * (1 + 0.1*Math.sin(rad));
+    var radEnter = rad * (1 - 0.1*Math.sin(rad));
+
+    var offsetExitX = Math.sin(radExit) * offset;
+    var offsetExitY = Math.cos(radExit) * offset;
+    var offsetEnterX = Math.sin(radEnter) * offset;
+    var offsetEnterY = Math.cos(radEnter) * offset;
+
+    var adjustedDeltaX = deltaX-offsetEnterX-offsetExitX;
+    var adjustedDeltaY = deltaY-offsetEnterY-offsetExitY;
+
+    return [
+      from.x+offsetExitX, from.y+offsetExitY,
+      (adjustedDeltaX/2+deltaY/8), (adjustedDeltaY/2-deltaX/8),
+      adjustedDeltaX, adjustedDeltaY
+    ];
+  };
+
+  var quadraticString = function(startX, startY, ctrlX, ctrlY, endX, endY) {
+    return [
+      "M",
+      [startX, startY].join(','),
+      "q",
+      [ctrlX, ctrlY].join(','),
+      [endX, endY].join(','),
+    ].join(' ');
+  };
+
+
+  window.curvedConnection = curvedConnection;
+  window.quadraticString = quadraticString;
   window.setStageSize = setSize;
   window.appendToSVG = appendTo;
   window.clearSVG = clear;
