0ae3f3e5ad5dd7e754b5205639da5c18b5555393
Laszlo Korte <me@laszlokorte.de>
2015-12-02 22:46:55 +0100

    draw arrow heads

--- common.js
+++ common.js
@@ -33,23 +33,55 @@
     var deltaY = to.y - from.y;
 
     var rad = Math.atan2(deltaX, deltaY);
-    var radExit = rad * (1 + 0.1*Math.sin(rad));
-    var radEnter = rad * (1 - 0.1*Math.sin(rad));
+    var radExit = rad * (1 + 0.2*Math.sin(rad));
+    var radEnter = rad * (1 - 0.2*Math.sin(rad));
 
     var offsetExitX = Math.sin(radExit) * offset;
     var offsetExitY = Math.cos(radExit) * offset;
-    var offsetEnterX = Math.sin(radEnter) * offset;
-    var offsetEnterY = Math.cos(radEnter) * offset;
+    var offsetEnterX = Math.sin(radEnter) * (offset + 20);
+    var offsetEnterY = Math.cos(radEnter) * (offset + 20);
 
     var adjustedDeltaX = deltaX-offsetEnterX-offsetExitX;
     var adjustedDeltaY = deltaY-offsetEnterY-offsetExitY;
 
+    var distance = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+    var bending = distance/(8000);
+
+
     return [
       from.x+offsetExitX, from.y+offsetExitY,
-      (adjustedDeltaX/2+deltaY/8), (adjustedDeltaY/2-deltaX/8),
+      (adjustedDeltaX/2+deltaY*bending), (adjustedDeltaY/2-deltaX*bending),
       adjustedDeltaX, adjustedDeltaY
     ];
   };
+
+  var interpQuadratric = function(t, s, c, e) {
+    var tInv = (1 - t);
+    return tInv * tInv * s + 2 * tInv * t * c + t * t * e;
+  }
+
+  var curveArrowHead = function(curve, length) {
+
+    var cx = interpQuadratric(0.8, 0, curve[2], curve[4]);
+    var cy = interpQuadratric(0.8, 0, curve[3], curve[5]);
+
+    var angle = Math.atan2(curve[4] - cx, curve[5] - cy);
+    var extend = Math.PI/4;
+
+    var angleA = angle + extend / 2;
+    var angleB = angle - extend / 2;
+    var tipX = curve[0] + curve[4] + Math.sin(angle) * length/2;
+    var tipY = curve[1] + curve[5] + Math.cos(angle) * length/2;
+
+    return [
+      tipX,
+      tipY,
+      tipX - Math.sin(angleA) * length,
+      tipY - Math.cos(angleA) * length,
+      tipX - Math.sin(angleB) * length,
+      tipY - Math.cos(angleB) * length,
+    ];
+  }
 
   var quadraticString = function(startX, startY, ctrlX, ctrlY, endX, endY) {
     return [
@@ -64,6 +96,7 @@
 
   window.curvedConnection = curvedConnection;
   window.quadraticString = quadraticString;
+  window.curveArrowHead = curveArrowHead;
   window.setStageSize = setSize;
   window.appendToSVG = appendTo;
   window.clearSVG = clear;
